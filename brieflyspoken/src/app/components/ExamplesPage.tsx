4f7e261f705726f8176065cd0087d4327ea628a8
Ivan Radev <107320114+R-zine@users.noreply.github.com>
2024-05-15 10:53:56 +0300

    added loader, favicon

--- brieflyspoken/src/app/components/ExamplesPage.tsx
+++ brieflyspoken/src/app/components/ExamplesPage.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-import { Fragment, useCallback, useState } from "react";
+import { Fragment, useCallback, useEffect, useState } from "react";
 import './ExamplesPage.css'
 
 // Wouldn't work with Chrome unless I moved this function outside of the component
 const speak = (text: string, setIsSpeaking: Function) => {
     if ('speechSynthesis' in window) {
         const synthesis = window.speechSynthesis;
-      
-        const speakText = function() {
+
+        const speakText = function () {
             // Chrome has a 15-second audio cutoff either way...
             const utterance = new SpeechSynthesisUtterance(text.substring(0, 3600));
-            
+
             utterance.lang = 'en-US';
             utterance.volume = 1; // 0 to 1
             utterance.rate = 1; // 0.1 to 10
@@ -19,10 +19,10 @@ const speak = (text: string, setIsSpeaking: Function) => {
 
             utterance.onstart = () => setIsSpeaking(true);
             utterance.onend = () => setIsSpeaking(false);
-            
+
             synthesis.speak(utterance);
         };
-      
+
         if (synthesis.getVoices().length !== 0) {
             window.speechSynthesis.cancel();
             speakText();
@@ -33,9 +33,9 @@ const speak = (text: string, setIsSpeaking: Function) => {
                 speakText();
             };
         }
-      } else {
+    } else {
         console.log('Speech synthesis is not supported in this browser.');
-      }
+    }
 }
 
 export const ExamplesPage = ({ data }: any) => {
@@ -50,17 +50,21 @@ export const ExamplesPage = ({ data }: any) => {
         }
     }, [data.originalText, isSpeaking])
 
+    useEffect(() => {
+        return () => window.speechSynthesis.cancel()
+    }, [])
+
     return (
-        <div className="examples-wrapper">  
+        <div className="examples-wrapper">
             {data.originalText && (
                 <div>
                     <h2>Briefing Text</h2>
                     <article>
                         {data.originalText.split('\n').map((line: string, index: number) => (
-                          <Fragment key={index}>
-                            {line}
-                            <br />
-                          </Fragment>
+                            <Fragment key={index}>
+                                {line}
+                                <br />
+                            </Fragment>
                         ))}
                     </article>
                     <button onClick={() => toggleGenericAudio()}>{isSpeaking ? 'Stop' : 'Listen to'} Generic, Non-AI Audio</button>
@@ -69,11 +73,11 @@ export const ExamplesPage = ({ data }: any) => {
             <section>
                 <h2>Audio Transcript</h2>
                 <article>{data.content.split('\n').map((line: string, index: number) => (
-                          <Fragment key={index}>
-                            {line}
-                            <br />
-                          </Fragment>
-                        ))}</article>
+                    <Fragment key={index}>
+                        {line}
+                        <br />
+                    </Fragment>
+                ))}</article>
                 <audio src={`https://nnnvprbeyjkayamcbbxe.supabase.co/storage/v1/object/public/audio-storage/${data.audioId}`} controls />
             </section>
         </div>
--- brieflyspoken/src/app/components/HomePage.tsx
+++ brieflyspoken/src/app/components/HomePage.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { DataGrid } from '@mui/x-data-grid';
+import { CircularProgress } from '@mui/material';
 import { useRouter } from 'next/navigation';
 import './HomePage.css'
+import { useState, Fragment } from 'react';
 
 const columns: any[] = [
-    { field: 'id', headerName: 'ID', width: 90,  },
+    { field: 'id', headerName: 'ID', width: 90, },
     {
         field: 'originalText',
         headerName: 'Original Text',
@@ -14,40 +16,55 @@ const columns: any[] = [
     {
         field: 'content',
         headerName: 'Content',
-        flex: 1, 
+        flex: 1,
     },
     {
         field: 'isCustomVoice',
         headerName: 'Custom Voice',
-        width: 100, 
+        width: 100,
     },
     {
         field: 'isTwo',
         headerName: 'Podcast?',
-        width: 100, 
+        width: 100,
     }
 
 ];
 
 const HomePage = ({ entries }: any) => {
 
+    const [isLoading, setIsLoading] = useState(false)
+
     const router = useRouter()
+
+    if (isLoading) return (<Fragment>
+        <svg width={0} height={0}>
+            <defs>
+                <linearGradient id="my_gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+                    <stop offset="0%" stopColor="rgb(178, 224, 200)" />
+                    <stop offset="100%" stopColor="rgb(255, 130, 0)" />
+                </linearGradient>
+            </defs>
+        </svg>
+        <CircularProgress size={100} sx={{ 'svg circle': { stroke: 'url(#my_gradient)' }, position: "absolute", top: "calc(50vh - 50px)" }} />
+    </Fragment>)
 
     return (
         <section style={{ width: '100%' }}>
-            <DataGrid 
-                columns={columns} 
+            <DataGrid
+                columns={columns}
                 rows={entries.map((entry: any) => ({
                     ...entry,
                     ...Object.fromEntries(
-                      Object.entries(entry).map(([key, value]) => [
-                        key,
-                        typeof value === 'boolean' ? (value ? '✓' : '✗') : value,
-                      ])
+                        Object.entries(entry).map(([key, value]) => [
+                            key,
+                            typeof value === 'boolean' ? (value ? '✓' : '✗') : value,
+                        ])
                     ),
-                  }))} 
+                }))}
                 onRowClick={(row) => {
                     router.push(`/examples/${row.id}`)
+                    setIsLoading(true)
                 }}
                 sx={{
                     boxShadow: 2,
